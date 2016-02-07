f6ff1a138dca7bfb76b6605a259a0f8eafe06da8
Tomas Trescak <tomi.trescak@gmail.com>
2016-02-07 19:25:19 +1100

    Changes to reflect the README

--- modules/Mantra-Base/client/main.js
+++
@@ -1,8 +0,0 @@
-import loadMethodStubs from './configs/method_stubs';
-import {initContext} from './configs/context';
-import {initRoutes} from './configs/routes.jsx';
-import actions from './actions';
-
-loadMethodStubs();
-const context = initContext();
-initRoutes(context, actions);
